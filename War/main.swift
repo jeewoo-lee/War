cd06858ba0c0373f1f9d36b4064ed8984d440e6c
Jeewoo Lee <jlee@lcs.on.ca>
2020-02-12 19:19:14 -0500

    Initial Commit

---
+++ War/main.swift
@@ -0,0 +1,12 @@
+//
+//  main.swift
+//  War
+//
+//  Created by Lee, Jeewoo on 2020-02-12.
+//  Copyright © 2020 Lee, Jeewoo. All rights reserved.
+//
+
+import Foundation
+
+print("Hello, World!")
+
